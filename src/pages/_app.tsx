c19c6cc00fd76ff2e23a893b8403deb0895e234e
Rafael Morais <rafaelsilvademorais@gmail.com>
2023-05-31 15:44:55 -0300

    Testing deploy railway

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
-import Header from "@/components/Header";
+import Header from "./../components/Header";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({
